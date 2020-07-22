bf3acd77790233f19a10edabbe395ee50ddd56af
thelegendman26 <teerapong.pu@baac.or.th>
2020-07-22 15:51:59 +0700

    com2

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const connectDB = require('./config/db')
 
 const userRoute = require('./routes/userRoute');
 const postRoute = require('./routes/postRoute');
+const userFaceRoute = require('./routes/userFaceRoute');
 
 const app = express();
 
@@ -55,8 +56,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 //init passport
 app.use(passport.initialize());
 
-app.use('/api/user', userRoute);
-app.use('/api/post', postRoute);
+// app.use('/api/user', userRoute);
+// app.use('/api/post', postRoute);
+app.use('/singin', userFaceRoute);
 
 app.use(errorHandler);
 
--- models/userFaceModel.js
+++ models/userFaceModel.js
@@ -1,29 +1,15 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
-const userSchema = Schema({
-  name:  { type: String, required: true, trim: true },
-  email: { type: String, required: true, trim: true, unique: true, index: true },
-  password: { type: String, required: true, trim: true , minlength: 3 },
-  role: { type: String, default: 'member' }
+const userFaceSchema = Schema({
+  username:  { type: String, required: true, trim: true },
+  password: { type: String, required: true, trim: true  },
+  user_id: { type: Number, required: true, trim: true  }
 },{
-  collection: 'users'
+  collection: 'User_Authen'
 });
 
-userSchema.methods.encryptPassword = async (password)  => {
-  const salt = await bcrypt.genSalt(10);
-  const hashPassword = await bcrypt.hash(password, salt);
-  return hashPassword;
-}
-
-userSchema.methods.comparePassword = async function (password)  {
-  console.log(password);
-  console.log(this.password);
-  const isValid = await bcrypt.compare(password, this.password);
-  return isValid;
-}
   
-  const user = mongoose.model('User', userSchema);
+  const user_authen = mongoose.model('user_authen', userFaceSchema);
   
-  module.exports = user;;
+  module.exports = user_authen;

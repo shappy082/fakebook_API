733be3f9fed3899322a385bb7c987eaa77a52987
shappy082 <papawich.int@gmail.com>
2020-08-01 17:35:25 +0700

    update friend request api

--- models/friendRequestModel.js
+++ models/friendRequestModel.js
@@ -3,12 +3,13 @@ const Schema = mongoose.Schema;
 
 const schema = new Schema(
   {
-    user_id: { type: String, required: true, trim: true },
-    user_id_req: { type: String, trim: true },
+    user_id: { type: Number, required: true },
+    user_id_req: { type: Number, required: true },
     flag_submit: { type: Number, default: 0 },
   },
   {
     collection: "Friend_Req",
+    versionKey: false,
   }
 );
 const friendRequest = mongoose.model("Friend_Req", schema);
